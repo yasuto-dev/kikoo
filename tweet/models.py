91ec33a3dacc447c0a9c55888d65fb71cb028c42
yasuto-dev <yasutomisono@gmail.com>
2021-03-13 12:39:20 +0900

    add

--- tweet/models.py
+++ tweet/models.py
@@ -5,10 +5,9 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
-    likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.content
